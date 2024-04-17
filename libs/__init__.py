2c9707cb295514869738baa78082bf61bda19429
Anand Phulwani <anand_phulwani@yahoo.com>
2024-04-18 02:55:07 +0530

    Added `config.ini` and `config_loader.py` for config system.

---
+++ libs/__init__.py
@@ -0,0 +1,2 @@
+# lib/__init__.py
+# This can be empty or can have package initialization code
---
+++ libs/config_loader.py
@@ -0,0 +1,37 @@
+# lib/config_loader.py
+import configparser
+import math
+
+def convert_to_appropriate_type(value):
+    """Try converting strings to integers or floats when possible."""
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value  # Return as string if not an int or float
+
+
+def load_config(filename):
+    """Load and return configuration settings with type-inferred values."""
+    config = configparser.ConfigParser()
+    config.read(filename)
+    
+    config_items = config.items('DEFAULT')
+
+    config_dict = {}
+    for key, value in config_items:
+        config_dict[key] = convert_to_appropriate_type(value)
+    if 'padding' in config_dict and 'buffer_padding' in config_dict and 'frame_width' in config_dict and 'frame_height' in config_dict:
+        config_dict['start_width'] = int(config_dict['padding']) + int(config_dict['buffer_padding'])
+        config_dict['end_width'] = int(config_dict['frame_width']) - int(config_dict['padding']) - int(config_dict['buffer_padding'])
+
+        config_dict['start_height'] = int(config_dict['padding']) + int(config_dict['buffer_padding'])
+        config_dict['end_height'] = int(config_dict['frame_height']) - int(config_dict['padding']) - int(config_dict['buffer_padding'])
+
+        config_dict['usable_width'] = ( config_dict['end_width'] - config_dict['start_width'] ) // 2
+        config_dict['usable_height'] = ( config_dict['end_height'] - config_dict['start_height'] ) // 2
+
+        config_dict['bits_per_frame'] = math.floor((config_dict['usable_width'] * config_dict['usable_height']) / 8) * 8
+    return config_dict
