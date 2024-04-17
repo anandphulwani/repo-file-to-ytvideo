3be324c60ddaff36340bca20ee7ca07908c9906d
Anand Phulwani <anand_phulwani@yahoo.com>
2024-04-18 00:45:06 +0530

    Added `sandbox` to try random scripts.

---
+++ sandbox_tryrandom_scripts/test04.py
@@ -0,0 +1,16 @@
+from PIL import Image, ImageDraw, ImageFont
+
+width, height = 640, 480
+background_color = (255, 255, 255)
+text_color = (0, 0, 0)
+font_size = 40
+
+frames = []
+for i in range(100):  # Create 100 frames
+    img = Image.new('RGB', (width, height), color = background_color)
+    d = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", font_size)
+    d.text((10+i, 220), "Hello, World!", fill=text_color, font=font)
+    frames.append(img)
+
+frames[0].save('text_animation.gif', save_all=True, append_images=frames[1:], optimize=False, duration=40, loop=0)
