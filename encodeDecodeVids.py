cc1717323d81b3ea3390fe909d71650cd0aac02d
Anand Phulwani <anand_phulwani@yahoo.com>
2024-04-18 00:40:29 +0530

    Some major changes

--- encodeDecodeVids.py
+++ encodeDecodeVids.py
@@ -1,190 +1,206 @@
 from PIL import Image
 import math
 import os
+import base64
+import sys
+import json
 from moviepy.editor import ImageSequenceClip
-import imageio
 import numpy as np
 from PIL import Image
-import io
 from tqdm import tqdm
-from pytube import YouTube
+import cv2
 
+def detect_base_from_json(encoding_map_path):
+    # Load and check the JSON encoding map
+    with open(encoding_map_path, 'r') as file:
+        encoding_map = json.load(file)
+    base = len(encoding_map)
 
-def file_to_binary():
-    # get file size
-    dir_path = os.getcwd()
-    for file_name in os.listdir(dir_path):
-        if file_name.endswith('.mkv'):
-            print(file_name)
-            file_name = file_name
-            break
-    file_size = os.path.getsize(file_name)
+    # Map the base to its corresponding function
+    base_functions = {
+        2: bin,
+        8: oct,
+        10: lambda x: str(x),
+        16: hex,
+        64: lambda x: base64.b64encode(x).decode('utf-8')
+    }
 
-    # read file as binary and convert to string of 0's and 1's
-    binary_string = ""
-    with open(file_name, "rb") as f:
-        for chunk in tqdm(iterable=iter(lambda: f.read(1024), b""), total=math.ceil(file_size/1024), unit="KB"):
-            binary_string += "".join(f"{byte:08b}" for byte in chunk)
+    if base in base_functions:
+        return base, base_functions[base]
+    else:
+        print("Unsupported base detected in JSON encoding map.")
+        sys.exit(1)
 
-    return binary_string
+def file_to_encodeddata(file_path, encoding_map_path='encoding_color_map.json'):
+    base, base_function = detect_base_from_json(encoding_map_path)
 
-    # write binary string to text file
+    print(f"Base is {base}")
 
+    # Check if the file exists
+    if not os.path.exists(file_path):
+        print("The specified file does not exist.")
+        sys.exit(1)
 
-def binary_to_video(bin_string, width=1920, height=1080, pixel_size=4, fps=24):
-    # Calculate the total number of pixels needed to represent the binary string
-    num_pixels = len(bin_string)
+    # Read the file content
+    with open(file_path, "rb") as file:
+        file_content = file.read()
 
-    # Calculate the number of pixels that can fit in one image
-    pixels_per_image = (width // pixel_size) * (height // pixel_size)
+    # Convert the file content based on the detected base
+    if base == 64:  # Directly encode for base64
+        encoded_data = base_function(file_content)
+    else:
+        # For other bases, encode each byte individually
+        encoded_data = "".join(base_function(byte)[2:] for byte in file_content)
 
-    # Calculate the number of images needed to represent the binary string
-    num_images = math.ceil(num_pixels / pixels_per_image)
+    return encoded_data
 
-    # Create an array to store the frames
+def encodeddata_to_frames(file_path='fdm.exe', encoding_map_path='encoding_color_map.json', width=1920, height=1080, pixel_size=1, fps=1):
+    encoded_data = file_to_encodeddata(file_path, encoding_map_path)
+    base, base_function = detect_base_from_json(encoding_map_path)
+    
+    # Open the file in write mode and save the encoded data
+    with open(f"{file_path}_stream.txt", "w") as file:
+        file.write(encoded_data)
+        
+    # Load the encoding color map
+    with open(encoding_map_path, 'r') as file:
+        encoding_color_map = json.load(file)
+
+    num_elements = len(encoded_data) # Calculate the total number of elements needed to represent the encoded data    
+    elements_per_image = (width // pixel_size) * (height // pixel_size) # Calculate the number of elements that can fit in one image
+    num_images = math.ceil(num_elements / elements_per_image) # Calculate the number of images needed
     frames = []
-
+    
     # Loop through each image
     for i in tqdm(range(num_images)):
-        # Calculate the range of binary digits needed for this image
-        start_index = i * pixels_per_image
-        end_index = min(start_index + pixels_per_image, num_pixels)
-        binary_digits = bin_string[start_index:end_index]
-
-        # Create a new image object with the given size
+        # Determine the part of the encoded data to use for this image
+        start_index = i * elements_per_image
+        end_index = min(start_index + elements_per_image, num_elements)
+        part_data = encoded_data[start_index:end_index]
+        
+        # Create a new image
         img = Image.new('RGB', (width, height), color='white')
+        
+        # Fill the image with encoded data
+        for j, char in enumerate(part_data):
+            # Determine the color of the pixel
+            if char in encoding_color_map:
+                color = tuple(int(encoding_color_map[char][k:k+2], 16) for k in (1, 3, 5))
+            elif char == '=' and base == 64:
+                # Handle padding character for base64 specifically
+                color = (0, 0, 0)
+            else:
+                print(f"Unknow character:{char} found in encoded data stream")
+                sys.exit(1)
+            
+            # Calculate pixel position
+            x = (j % (width // pixel_size)) * pixel_size
+            y = (j // (width // pixel_size)) * pixel_size
+            
+            # Draw the pixel
+            for px in range(pixel_size):
+                for py in range(pixel_size):
+                    img.putpixel((x + px, y + py), color)
+        
+        # Convert PIL image to numpy array and add to frames
+        frame = np.array(img)
+        # frames.append(frame)
+        yield frame
 
-        # Loop through each row of binary digits
-        for row_index in range(height // pixel_size):
+def inject_frames_to_outvideo(video_path = "animation_3600.mp4", output_path = "animation_3600_mod.mp4", framegenfn = encodeddata_to_frames(), skip_seconds=0, original_fps=24, new_fps=30):
+    # Open the source video
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        raise IOError("Error opening video stream or file")
 
-            # Get the binary digits for the current row
-            start_index = row_index * (width // pixel_size)
-            end_index = start_index + (width // pixel_size)
-            row = binary_digits[start_index:end_index]
+    # Define the codec and create VideoWriter object
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), new_fps, (frame_width, frame_height))
 
-            # Loop through each column of binary digits
-            for col_index, digit in enumerate(row):
+    # Calculate the number of frames to skip and position the video
+    # frames_to_skip = skip_seconds * original_fps
+    # cap.set(cv2.CAP_PROP_POS_FRAMES, frames_to_skip)
 
-                # Determine the color of the pixel based on the binary digit
-                if digit == '1':
-                    color = (0, 0, 0)  # Black
-                else:
-                    color = (255, 255, 255)  # White
+    # Variables to control the injection of new frames
+    frame_counter = 0
+    isinjectedframedone = False
 
-                # Calculate the coordinates of the pixel
-                x1 = col_index * pixel_size
-                y1 = row_index * pixel_size
-                x2 = x1 + pixel_size
-                y2 = y1 + pixel_size
+    # Read and process the video
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break  # Stop if end of video reached
 
-                # Draw the pixel on the image
-                img.paste(color, (x1, y1, x2, y2))
+        # Write the frame to the output video
+        out.write(frame)
+        frame_counter += 1
+        # print(f"{frame_counter}: remainderVal: {frame_counter % 30}, remainder: {frame_counter % 30 in [5, 15, 25]}")
+        # print(f"{frame_counter}: firstCondition: {injected_frame_counter < len(frames_to_inject)}, injected_frame_counter: {injected_frame_counter}, len(frames_to_inject) {len(frames_to_inject)}")
 
-        # Add the frame to the list of frames
-        with io.BytesIO() as f:
-            img.save(f, format='PNG')
-            frame = np.array(Image.open(f))
-        frames.append(frame)
+        if not isinjectedframedone and frame_counter % 30 in [12]:
+            new_frame = next(framegenfn, None)
+            if new_frame is not None:
+                # print(f"injected a frame at {frame_counter}")
+            # Check if it's time to inject a frame
+            # if injected_frame_counter < len(frames_to_inject) and frame_counter % 30 in [5, 15, 25]:
+                # print(f"injected_frame_counter: {injected_frame_counter}, len(frames_to_inject) {len(frames_to_inject)}")
+                # Assuming frames_to_inject is a generator or a list of frames
+                # img = frames_to_inject[injected_frame_counter]
+                # Inject the frame and its duplicate
+                out.write(new_frame)
+                # out.write(new_frame)
+                # injected_frame_counter += 1
+                # frame_counter += 2  # Update counter to account for the injected frames
+            else:
+                print("injected frame done.")
+                isinjectedframedone = True
+        if isinjectedframedone and frame_counter % 30 == 0:
+            print("injected frame done and second done.")
+            break
 
-    # Create a video from the frames using MoviePy
-    clip = ImageSequenceClip(frames, fps=fps)
-
-    # Write the video to a file
-    clip.write_videofile('video.mp4', fps=fps)
-
-
-def process_images(frames):
-    # Define the threshold value for determining black pixels
-    threshold = 128
-
-    # Create an empty string to store the binary digits
-    binary_digits = ''
-
-    # Loop through each frame in the list
-    for frame in tqdm(frames, desc="Processing frames"):
-        # Convert the frame to grayscale
-        gray_frame = np.mean(frame, axis=2).astype(np.uint8)
-
-        # Hardcode the pixel size to 4
-        pixel_size = 4
-
-        # Loop through each row of pixels
-        for y in range(0, gray_frame.shape[0], pixel_size):
-            # Loop through each column of pixels
-            for x in range(0, gray_frame.shape[1], pixel_size):
-                # Get the color of the current pixel
-                color = gray_frame[y:y+pixel_size, x:x+pixel_size]
-
-                # Determine the binary digit based on the color of the pixel
-                if color.mean() < threshold:
-                    binary_digits += '1'
-                else:
-                    binary_digits += '0'
-
-    # Store the binary string in a single text file
-    return binary_digits
-
-def binaryToFile(binary_filename):
-    # convert binary string to binary data
-    binary_data = bytes(int(binary_filename[i:i+8], 2)
-                        for i in range(0, len(binary_filename), 8))
-
-    # write binary data to output file
-    with open("reverse.mkv", "wb") as f:
-        with tqdm(total=len(binary_data), unit='B', unit_scale=True, desc="Writing binary data") as pbar:
-            for chunk in range(0, len(binary_data), 1024):
-                f.write(binary_data[chunk:chunk+1024])
-                pbar.update(1024)
-
-        print(f"Binary data converted to example_reverse.zip")
-
-
-def ExtractFrames():
-    am = []
-    dir_path = os.getcwd()
-    files = [f for f in os.listdir(dir_path) if f.endswith('.webm')]
-    files = [f for f in os.listdir(dir_path) if f.endswith('.mp4')]
-    files = files[0]
-    # Open the video file
-    vid = imageio.get_reader(files, 'ffmpeg')
-
-    # Get the fps of the video
-    fps = vid.get_meta_data()['fps']
-
-    # Get the total number of frames in the video
-    num_frames = vid.get_length()
-
-    # Use tqdm to create a progress bar
-    with tqdm(total=num_frames) as pbar:
-        # Iterate over every frame of the video
-        for i, frame in enumerate(vid):
-            # Append the frame to the list
-            am.append(frame)
-            # Update the progress bar
-            pbar.update(1)
-
-    # Return the list of frames
-    return am
-
-def youtube_video_downloader(url):
-    # create YouTube object
-    yt = YouTube(url)
-
-    # get the video with 1080p resolution
-    stream = yt.streams.filter(res="1080p").first()
-
-    # download the video
-    stream.download()
+    # Release everything if the job is finished
+    print("It is done")
+    cap.release()
+    out.release()
 
 
-input_Data = input(
-    "convert file to video press 1 and Enter  \nconvert video to file press 2  and Enter\nDownload video from youtube press 3 and Enter\n")
 
-if (input_Data == "1"):
-    binary_to_video(file_to_binary())
-elif (input_Data == "2"):
-    binaryToFile(process_images(ExtractFrames()))
-elif (input_Data == "3"):
-    youtube_video_downloader(input("Enter Url "))
-else:
-    print("404")
+# Example usage
+# video_path = 
+# output_path = 
+
+# try:
+#     inject_frames(video_path, output_path, frames)
+#     print("Video processing completed.")
+# except IOError as e:
+#     print(str(e))
+
+if __name__ == "__main__":
+    # Ask the user for the path to the file they wish to encode
+    # file_path = input("Please enter the file path to encode: ")
+
+    # Optional: Ask for the path to the encoding color map JSON file
+    # This step can be skipped if you always use a default path for the encoding map
+    encoding_map_path = input("Please enter the path to the encoding color map JSON file (press enter to use default): ")
+    if not encoding_map_path.strip():
+        encoding_map_path = 'encoding_color_map.json'  # Default path
+
+    # Encode the file
+    # try:
+    inject_frames_to_outvideo()
+    
+    # encoded_data = file_to_encodeddata(file_path, encoding_map_path)
+    # if encoded_data:
+    #     print("File successfully encoded.")
+    #     inject_frames_to_outvideo()
+    #     encodeddata_to_video(encoded_data, file_path)
+    # else:
+    #     print("No encoded data was returned.")
+    
+    #except FileNotFoundError:
+    #    print(f"File not found: {file_path}")
+    #    sys.exit(1)
+    #except Exception as e:
+    #    print(f"An error occurred: {e}")
+    #    sys.exit(1)
