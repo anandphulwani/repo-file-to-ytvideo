89e6fe92002db760c27eca22eff3357480fc8821
Anand Phulwani <anand_phulwani@yahoo.com>
2025-01-13 01:32:41 +0530

    Remove unused fn `encodeddata_to_file`.

--- libs/file_codec.py
+++ libs/file_codec.py
@@ -1,48 +1,10 @@
 from tqdm import tqdm
-import base64
 import os
 import sys
 import math
 import hashlib
 from .detect_base_from_json import detect_base_from_json
 
-def encodeddata_to_file(encoded_data, video_path, encoding_map_path='encoding_color_map.json'):
-    base = detect_base_from_json(encoding_map_path)
-    if base == 64:
-        binary_data = base64.b64decode(encoded_data)
-    else:
-        base_functions = {
-            2: lambda x: int(x, 2),
-            8: lambda x: int(x, 8),
-            10: lambda x: int(x, 10),
-            16: lambda x: int(x, 16),
-        }
-        
-        # Determine the size of each segment based on the base, because the encoded data length for each byte can vary with base
-        segment_length = {
-            2: 8,  # 1 byte = 8 bits
-            8: 3,  # 1 byte = up to 3 octal digits
-            10: 3, # This is more complex as it doesn't align neatly; likely needs special handling
-            16: 2,  # 1 byte = 2 hexadecimal digits
-        }
-
-        if base == 10: # Special handling for base 10 (variable length encoding)
-            # Assuming decimal encoded data is separated by a non-numeric character, e.g., ","
-            # This is a simplification; real implementation might need to consider how data was encoded
-            segments = encoded_data.split(',')
-            binary_data = bytes(int(segment) for segment in segments)
-        else:
-            binary_data = bytes(base_functions[base](encoded_data[i:i+segment_length[base]]) for i in range(0, len(encoded_data), segment_length[base]))
-
-    # Write binary data to output file
-    with open(f"file_reverse.exe", "wb") as f:
-        with tqdm(total=len(binary_data), unit='B', unit_scale=True, desc=f"Writing binary data to {video_path}_reverse.rev") as pbar:
-            for chunk in range(0, len(binary_data), 1024):
-                f.write(binary_data[chunk:chunk+1024])
-                pbar.update(min(1024, len(binary_data) - chunk))
-
-    print(f"Encoded data converted back to {video_path}_reverse.rev")
-     
 def file_to_encodeddata(config, file_path, debug = False):
     bits_per_frame = config['bits_per_frame']
     
