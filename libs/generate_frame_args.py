6a32f73bb145daa9f2b20377721fd9ebf152ab27
Anand Phulwani <anand_phulwani@yahoo.com>
2025-01-31 22:16:56 +0530

    Added `debug` parameter in `generate_frame_args` fn.

--- libs/generate_frame_args.py
+++ libs/generate_frame_args.py
@@ -1,4 +1,4 @@
-def generate_frame_args(cap, config, frame_data_iter, encoding_color_map):
+def generate_frame_args(cap, config, frame_data_iter, encoding_color_map, debug):
     frame_index = 0
     while True:
         ret, frame = cap.read()
@@ -9,7 +9,7 @@ def generate_frame_args(cap, config, frame_data_iter, encoding_color_map):
             content_type, frame_data = next(frame_data_iter)
             if frame_data is None:
                 break
-            yield (frame, config, encoding_color_map, frame_data, frame_index, content_type)
+            yield (frame, config, encoding_color_map, frame_data, frame_index, content_type, debug)
             frame_index += 1
         except StopIteration:
             break
