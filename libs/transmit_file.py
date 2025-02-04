724856e00576420576850c9ddd9443ac490ef0c0
Anand Phulwani <anand_phulwani@yahoo.com>
2025-02-04 21:58:38 +0530

    Added `Metadata.py` and `PreMetadata.py` classes and removed `transmit_file.py` class as it is redundant.

--- libs/transmit_file.py
+++
@@ -1,7 +0,0 @@
-class transmit_file:
-
-    def __init__(self, name, size, binary_length, sha1):
-        self.name = name
-        self.size = int(size)
-        self.binary_length = int(binary_length)
-        self.sha1 = sha1
