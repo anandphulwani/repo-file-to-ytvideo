b02a6d4c4642964fdcac0bdbd4ca28ed9eb38da5
Anand Phulwani <anand_phulwani@yahoo.com>
2025-02-09 18:50:39 +0530

    Updated `generate_frame_args` so can use the same function for decoding.

--- libs/generate_frame_args.py
+++ libs/generate_frame_args.py
@@ -1,15 +1,15 @@
-def generate_frame_args(cap, config, frame_data_iter, encoding_color_map, debug):
+def generate_frame_args(cap, config, frame_data_iter, encoding_color_map, debug, start_index=0, frame_step=1):
     frame_index = 0
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
         try:
-            content_type, frame_data = next(frame_data_iter)
-            if frame_data is None:
-                break
-            yield (frame, config, encoding_color_map, frame_data, frame_index, content_type, debug)
-            frame_index += 1
+            if frame_index >= start_index and ((frame_index - start_index) % frame_step) == 0:
+                content_type, frame_data = next(frame_data_iter)
+                if frame_data is None:
+                    break
+                yield (frame, config, encoding_color_map, frame_data, frame_index, content_type, debug)
         except StopIteration:
             break
+        frame_index += 1
