a5a569821a91a621a2297d615788b86c977e7827
Anand Phulwani <anand_phulwani@yahoo.com>
2025-02-24 21:44:57 +0530

    Added `produce_tasks.py`.

---
+++ libs/produce_tasks.py
@@ -0,0 +1,19 @@
+#############################################################################
+# A GENERATOR that yields tasks to the pool from the queue.
+#############################################################################
+def produce_tasks(frame_queue, stop_event, config_params, encoding_color_map, frame_step, total_baseN_length, num_frames, metadata_frames):
+    """
+    Takes items from the frame_queue (pushed by the reader thread),
+    and yields them in the format that process_frame_optimized(...) expects:
+       (config_params, frame_bgr, encoding_color_map, frame_index, frame_step, 
+        total_baseN_length, num_frames, metadata_frames)
+    """
+    while not stop_event.is_set():
+        item = frame_queue.get()
+        if item is None:
+            # End of stream
+            break
+        (frame_index, frame_bgr) = item
+
+        # Build the tuple for your worker function:
+        yield (config_params, frame_bgr, encoding_color_map, frame_index, frame_step, total_baseN_length, num_frames, metadata_frames)
