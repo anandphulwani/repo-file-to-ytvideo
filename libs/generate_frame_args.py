dcb2f9567922199c8ef68228c70d82b8f4fd8438
Anand Phulwani <anand_phulwani@yahoo.com>
2025-03-15 01:12:48 +0530

    Used `frame_queue` instead of `cap` in `generate_frame_args`.

--- libs/generate_frame_args.py
+++ libs/generate_frame_args.py
@@ -1,11 +1,11 @@
 def generate_frame_args(frame_queue, config, frame_data_iter, encoding_color_map, debug):
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
         try:
             content_type, frame_data = next(frame_data_iter)
             if frame_data is None:
+                break
+            frame = frame_queue.get()
+            if frame is None:
                 break
             yield (frame, config, encoding_color_map, frame_data, None, content_type, debug)
         except StopIteration:
