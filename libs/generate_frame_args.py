4b38e62feb8980701c1e0fb45899cc2ed3b7463f
Anand Phulwani <anand_phulwani@yahoo.com>
2025-03-16 00:21:37 +0530

    Auto indentation only

--- libs/generate_frame_args.py
+++ libs/generate_frame_args.py
@@ -4,10 +4,9 @@ def generate_frame_args(frame_queue, config, frame_data_iter, encoding_color_map
         if not ret:
             break
         try:
-                content_type, frame_data = next(frame_data_iter)
-                if frame_data is None:
-                    break
-                yield (frame, config, encoding_color_map, frame_data, None, content_type, debug)
+            content_type, frame_data = next(frame_data_iter)
+            if frame_data is None:
+                break
+            yield (frame, config, encoding_color_map, frame_data, None, content_type, debug)
         except StopIteration:
             break
-
